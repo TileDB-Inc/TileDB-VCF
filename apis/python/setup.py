c1b1acc219dd74d563b51cf2bf4b55034938a490
Tyler Denniston <tyler@tiledb.io>
2019-10-01 17:20:19 -0400

    Better bundling of native libraries for python, Spark. (#23)

--- apis/python/setup.py
+++ apis/python/setup.py
@@ -4,7 +4,7 @@
 #
 # The MIT License
 #
-# Copyright (c) 2018 TileDB, Inc.
+# Copyright (c) 2019 TileDB, Inc.
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -30,20 +30,13 @@
 
 from setuptools import setup, find_packages, Extension
 from setuptools.command.build_ext import build_ext
+from setuptools.command.bdist_egg import bdist_egg
+from wheel.bdist_wheel import bdist_wheel
 
+import multiprocessing
 import os
-import sys
-
-# Directory containing this file
-CONTAINING_DIR = os.path.abspath(os.path.dirname(__file__))
-
-# Relative path of the package source
-SRC_DIR = 'src/tiledbvcf'
-
-# Installation directory of libtiledbvcf (C API).
-# TODO : make this more portable
-LIBTILEDBVCF_INSTALL_DIR = os.path.abspath(
-    os.path.join(CONTAINING_DIR, '../../dist'))
+import shutil
+import subprocess
 
 
 class get_pybind_include(object):
@@ -60,25 +53,114 @@ class get_pybind_include(object):
         return pybind11.get_include(self.user)
 
 
-def get_libtiledbvcf_include():
-    return '{}/include'.format(LIBTILEDBVCF_INSTALL_DIR)
+class PathConfig(object):
+    """Helper class with some path information."""
+    # Directory containing this file
+    containing_dir = os.path.abspath(os.path.dirname(__file__))
+
+    # Path of the Python package source
+    pkg_src = 'src/tiledbvcf'
+
+    # Source directory of TileDB-VCF native library
+    native_lib_src_dir = '../../libtiledbvcf'
+
+    # Build directory of TileDB-VCF native library
+    native_lib_build_dir = '../../libtiledbvcf/build'
+
+    # Path where TileDB-VCF native library should be installed
+    native_lib_install_dirs = ['../../dist']
 
 
-def get_libtiledbvcf_lib():
-    libdirs = ['lib', 'lib64']
-    if os.name == 'posix' and sys.platform == 'darwin':
-        libname = 'libtiledbvcf.dylib'
-    else:
-        libname = 'libtiledbvcf.so'
-    for dir in libdirs:
-        path = os.path.join(LIBTILEDBVCF_INSTALL_DIR, dir, libname)
-        if os.path.exists(path):
-            return os.path.join(LIBTILEDBVCF_INSTALL_DIR, dir)
-    raise Exception('Could not locate libtiledbvcf native library')
+def find_libtiledbvcf():
+    p = PathConfig()
+    libdirs = ['lib']
+    libnames = ['libtiledbvcf.dylib', 'libtiledbvcf.so']
+    for root in p.native_lib_install_dirs:
+        for libdir in libdirs:
+            for libname in libnames:
+                path = os.path.abspath(os.path.join(root, libdir, libname))
+                if os.path.exists(path):
+                    return path
+    return None
 
 
-class BuildExt(build_ext):
-    """A custom build extension for adding compiler-specific options."""
+def build_libtiledbvcf():
+    p = PathConfig()
+
+    install_dir = p.native_lib_install_dirs[0]
+    build_dir = p.native_lib_build_dir
+    src_dir = p.native_lib_src_dir
+    os.makedirs(build_dir, exist_ok=True)
+
+    cmake_exe = os.environ.get('CMAKE', 'cmake')
+    cmake_cmd = [cmake_exe,
+                 '-DENABLE_ARROW_EXPORT=ON',
+                 '-DFORCE_EXTERNAL_HTSLIB=ON',
+                 '-DCMAKE_BUILD_TYPE=Release',
+                 src_dir]
+    build_cmd = ['make', '-j{}'.format(multiprocessing.cpu_count() or 2)]
+    install_cmd = ['make', 'install-libtiledbvcf']
+
+    subprocess.check_call(cmake_cmd, cwd=build_dir)
+    subprocess.check_call(build_cmd, cwd=build_dir)
+    subprocess.check_call(install_cmd, cwd=build_dir)
+
+
+def find_or_build_libtiledbvcf(setuptools_cmd):
+    # Get a handle to the extension module
+    tiledbvcf_ext = None
+    for ext in setuptools_cmd.distribution.ext_modules:
+        if ext.name == 'tiledbvcf.libtiledbvcf':
+            tiledbvcf_ext = ext
+            break
+
+    # Find the native library
+    lib_path = find_libtiledbvcf()
+    if lib_path is None:
+        build_libtiledbvcf()
+        lib_path = find_libtiledbvcf()
+        if lib_path is None:
+            raise Exception(
+                'Could not find native libtiledbvcf after building.')
+
+    # Update the extension module with correct paths.
+    lib_dir = os.path.dirname(lib_path)
+    inc_dir = os.path.abspath(os.path.join(lib_dir, '..', 'include'))
+    tiledbvcf_ext.library_dirs += [lib_dir]
+    tiledbvcf_ext.include_dirs += [inc_dir]
+
+    # Copy native libs into the package dir so they can be found by package_data
+    native_libs = [os.path.join(lib_dir, f) for f in os.listdir(lib_dir)]
+    p = PathConfig()
+    package_data = []
+    for obj in native_libs:
+        shutil.copy(obj, p.pkg_src)
+        package_data.append(os.path.basename(obj))
+
+    # Install shared libraries inside the Python module via package_data.
+    print('Adding to package_data: {}'.format(package_data))
+    setuptools_cmd.distribution.package_data.update({'tiledbvcf': package_data})
+
+
+def get_ext_modules():
+    p = PathConfig()
+    src_files = ['libtiledbvcf.cc', 'reader.cc', 'writer.cc']
+    src_files = [os.path.join(p.pkg_src, 'binding', f) for f in src_files]
+    ext_modules = [
+        Extension(
+            'tiledbvcf.libtiledbvcf',
+            src_files,
+            include_dirs=[get_pybind_include(), get_pybind_include(user=True)],
+            libraries=['tiledbvcf'],
+            library_dirs=[],
+            language='c++'
+        ),
+    ]
+    return ext_modules
+
+
+class BuildExtCmd(build_ext):
+    """Builds the Pybind11 extension module."""
 
     def build_extensions(self):
         opts = ['-std=c++11', '-g', '-O2']
@@ -92,27 +174,21 @@ class BuildExt(build_ext):
             ext.libraries.extend(pyarrow.get_libraries())
             ext.library_dirs.extend(pyarrow.get_library_dirs())
 
+        find_or_build_libtiledbvcf(self)
         build_ext.build_extensions(self)
 
 
-ext_src_files = ['{}/binding/{}'.format(SRC_DIR, filename) for filename in
-                 ['libtiledbvcf.cc', 'reader.cc', 'writer.cc']]
-ext_modules = [
-    Extension(
-        'tiledbvcf.libtiledbvcf',
-        ext_src_files,
-        include_dirs=[
-            get_pybind_include(),
-            get_pybind_include(user=True),
-            get_libtiledbvcf_include(),
-        ],
-        libraries=['tiledbvcf'],
-        library_dirs=[
-            get_libtiledbvcf_lib()
-        ],
-        language='c++'
-    ),
-]
+class BdistEggCmd(bdist_egg):
+    def run(self):
+        find_or_build_libtiledbvcf(self)
+        bdist_egg.run(self)
+
+
+class BdistWheelCmd(bdist_wheel):
+    def run(self):
+        find_or_build_libtiledbvcf(self)
+        bdist_wheel.run(self)
+
 
 setup(
     name='tiledbvcf',
@@ -131,8 +207,9 @@ setup(
     install_requires=[],
     tests_require=[],
     test_suite='tests',
-    ext_modules=ext_modules,
-    cmdclass={'build_ext': BuildExt},
+    ext_modules=get_ext_modules(),
+    cmdclass={'build_ext': BuildExtCmd, 'bdist_egg': BdistEggCmd,
+              'bdist_wheel': BdistWheelCmd},
     zip_safe=False,
     classifiers=[
         'Development Status :: 4 - Beta',
@@ -149,5 +226,6 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
 )
--- apis/python/src/tiledbvcf/__init__.py
+++ apis/python/src/tiledbvcf/__init__.py
@@ -1,4 +1,21 @@
 # Initialize pyarrow first.
 import pyarrow
 
+import ctypes
+import os
+import sys
+
+if sys.platform == 'darwin':
+    lib_name = 'libtiledbvcf.dylib'
+else:
+    lib_name = 'libtiledbvcf.so'
+
+try:
+    # Try loading the bundled native library.
+    lib_dir = os.path.dirname(os.path.abspath(__file__))
+    ctypes.CDLL(os.path.join(lib_dir, lib_name))
+except OSError as e:
+    # Otherwise try loading by name only.
+    ctypes.CDLL(lib_name)
+
 from .dataset import (ReadConfig, TileDBVCFDataset)
