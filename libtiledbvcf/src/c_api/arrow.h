aef2a1706c3fe5c8bfa567e32cc3fd6664578343
Isaiah Norton <isaiah@tiledb.com>
2020-01-29 13:20:25 +0000

    Fix bug in calculation of null bitmask buffer size
    This calculation needs to return the number of uint8 elements required to store a nullable bitmask
    for a given number of records, which is "ceil(num_records, 8)" using the internal ceil function.
    
    For #47 compatibility with arrow 0.15 release and current trunk, where additional consistency checks
    have been added.

--- libtiledbvcf/src/c_api/arrow.h
+++ libtiledbvcf/src/c_api/arrow.h
@@ -290,7 +290,7 @@ class Arrow {
     std::shared_ptr<arrow::Buffer> arrow_nulls;
     if (buffer_info.nullable)
       arrow_nulls = arrow::Buffer::Wrap(
-          buffer_info.bitmap_buff, ceil(num_data_elements, 8));
+          buffer_info.bitmap_buff, ceil(num_records, 8));
 
     if (buffer_info.list) {
       // List of var-len char attribute.
@@ -330,7 +330,7 @@ class Arrow {
     std::shared_ptr<arrow::Buffer> arrow_nulls;
     if (buffer_info.nullable)
       arrow_nulls = arrow::Buffer::Wrap(
-          buffer_info.bitmap_buff, ceil(num_data_elements, 8));
+          buffer_info.bitmap_buff, ceil(num_records, 8));
 
     std::shared_ptr<arrow::Array> values_array(
         new ArrayT(num_data_elements, arrow_values));
