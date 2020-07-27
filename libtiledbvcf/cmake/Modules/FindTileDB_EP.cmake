4f4b20e593ebbe4a37c014d04bc2c84f9e9f3308
Seth Shelnutt <Shelnutt2@gmail.com>
2020-07-27 18:46:04 -0400

    Revert "Disable TBB for testing"
    This reverts commit fa6acf01e9bc0013bea76a76c8ca45b65630aabf.

--- libtiledbvcf/cmake/Modules/FindTileDB_EP.cmake
+++ libtiledbvcf/cmake/Modules/FindTileDB_EP.cmake
@@ -57,7 +57,6 @@ else()
         -DTILEDB_S3=${TILEDB_S3}
         -DTILEDB_VERBOSE=ON
         -DTILEDB_SERIALIZATION=ON
-        -DTILEDB_TBB=OFF
       UPDATE_COMMAND ""
       INSTALL_COMMAND
         ${CMAKE_COMMAND} --build . --target install-tiledb
