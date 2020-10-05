6b7291df77f0dd554e6203ff1a918d0066a41952
Aaron Wolen <aaron@wolen.com>
2020-10-05 09:49:57 -0500

    Update to TileDB 2.1 final release

--- libtiledbvcf/cmake/Modules/FindTileDB_EP.cmake
+++ libtiledbvcf/cmake/Modules/FindTileDB_EP.cmake
@@ -47,8 +47,8 @@ else()
 
     ExternalProject_Add(ep_tiledb
       PREFIX "externals"
-      URL "https://github.com/TileDB-Inc/TileDB/archive/2.1.0-rc5.zip"
-      URL_HASH SHA1=379888ef9b0319c50bd142e340f2e32d02f8dcb6
+      URL "https://github.com/TileDB-Inc/TileDB/archive/2.1.0.zip"
+      URL_HASH SHA1=c5e278ab5fa55852e5d89372c34ccafb2dbd9602
       DOWNLOAD_NAME "tiledb.zip"
       CMAKE_ARGS
         -DCMAKE_INSTALL_PREFIX=${EP_INSTALL_PREFIX}
