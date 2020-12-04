b1422d03a94bbf7772ce0af7c6cde416f1c2adb0
Seth Shelnutt <Shelnutt2@gmail.com>
2020-12-04 15:41:51 -0500

    Format with black

--- apis/python/tests/test_dask.py
+++ apis/python/tests/test_dask.py
@@ -12,12 +12,12 @@ CONTAINING_DIR = os.path.abspath(os.path.dirname(__file__))
 
 # Test inputs directory
 TESTS_INPUT_DIR = os.path.abspath(
-    os.path.join(CONTAINING_DIR, '../../../libtiledbvcf/test/inputs'))
+    os.path.join(CONTAINING_DIR, "../../../libtiledbvcf/test/inputs")
+)
 
-dask_cluster = dask.distributed.LocalCluster(n_workers=2,
-                                             threads_per_worker=1,
-                                             memory_limit='4GB',
-                                             processes=False)
+dask_cluster = dask.distributed.LocalCluster(
+    n_workers=2, threads_per_worker=1, memory_limit="4GB", processes=False
+)
 dask_client = dask.distributed.Client(dask_cluster)
 
 
@@ -40,126 +40,244 @@ def _check_dfs(expected, actual):
 @pytest.fixture
 def test_ds():
     return tiledbvcf.Dataset(
-        os.path.join(TESTS_INPUT_DIR, 'arrays/v3/ingested_2samples'))
+        os.path.join(TESTS_INPUT_DIR, "arrays/v3/ingested_2samples")
+    )
 
 
 def test_basic_reads(test_ds):
     expected_df = pd.DataFrame(
-        {'sample_name': pd.Series(
-            ['HG00280', 'HG01762', 'HG00280', 'HG01762', 'HG00280',
-             'HG01762', 'HG00280', 'HG00280', 'HG00280', 'HG00280',
-             'HG00280', 'HG00280', 'HG00280', 'HG00280']),
-            'pos_start': pd.Series(
-                [12141, 12141, 12546, 12546, 13354, 13354, 13375, 13396,
-                 13414, 13452, 13520, 13545, 17319, 17480], dtype=np.int32),
-            'pos_end': pd.Series(
-                [12277, 12277, 12771, 12771, 13374, 13389, 13395, 13413,
-                 13451, 13519, 13544, 13689, 17479, 17486], dtype=np.int32)})
+        {
+            "sample_name": pd.Series(
+                [
+                    "HG00280",
+                    "HG01762",
+                    "HG00280",
+                    "HG01762",
+                    "HG00280",
+                    "HG01762",
+                    "HG00280",
+                    "HG00280",
+                    "HG00280",
+                    "HG00280",
+                    "HG00280",
+                    "HG00280",
+                    "HG00280",
+                    "HG00280",
+                ]
+            ),
+            "pos_start": pd.Series(
+                [
+                    12141,
+                    12141,
+                    12546,
+                    12546,
+                    13354,
+                    13354,
+                    13375,
+                    13396,
+                    13414,
+                    13452,
+                    13520,
+                    13545,
+                    17319,
+                    17480,
+                ],
+                dtype=np.int32,
+            ),
+            "pos_end": pd.Series(
+                [
+                    12277,
+                    12277,
+                    12771,
+                    12771,
+                    13374,
+                    13389,
+                    13395,
+                    13413,
+                    13451,
+                    13519,
+                    13544,
+                    13689,
+                    17479,
+                    17486,
+                ],
+                dtype=np.int32,
+            ),
+        }
+    )
 
     # Region partitions
-    dask_df = test_ds.read_dask(attrs=['sample_name', 'pos_start', 'pos_end'],
-                                region_partitions=10)
+    dask_df = test_ds.read_dask(
+        attrs=["sample_name", "pos_start", "pos_end"], region_partitions=10
+    )
     df = dask_df.compute()
     _check_dfs(expected_df, df)
 
     # Sample partitions (we have to sort to check the result)
-    dask_df = test_ds.read_dask(attrs=['sample_name', 'pos_start', 'pos_end'],
-                                sample_partitions=2)
-    df = dask_df.compute().sort_values('sample_name').reset_index(drop=True)
-    _check_dfs(expected_df.sort_values('sample_name').reset_index(drop=True),
-               df)
+    dask_df = test_ds.read_dask(
+        attrs=["sample_name", "pos_start", "pos_end"], sample_partitions=2
+    )
+    df = dask_df.compute().sort_values("sample_name").reset_index(drop=True)
+    _check_dfs(expected_df.sort_values("sample_name").reset_index(drop=True), df)
 
     # Both
-    dask_df = test_ds.read_dask(attrs=['sample_name', 'pos_start', 'pos_end'],
-                                region_partitions=10, sample_partitions=2)
-    df = dask_df.compute().sort_values('sample_name').reset_index(drop=True)
-    _check_dfs(expected_df.sort_values('sample_name').reset_index(drop=True),
-               df)
+    dask_df = test_ds.read_dask(
+        attrs=["sample_name", "pos_start", "pos_end"],
+        region_partitions=10,
+        sample_partitions=2,
+    )
+    df = dask_df.compute().sort_values("sample_name").reset_index(drop=True)
+    _check_dfs(expected_df.sort_values("sample_name").reset_index(drop=True), df)
 
     # No partitioning
-    dask_df = test_ds.read_dask(attrs=['sample_name', 'pos_start', 'pos_end'])
+    dask_df = test_ds.read_dask(attrs=["sample_name", "pos_start", "pos_end"])
     df = dask_df.compute()
     _check_dfs(expected_df, df)
 
 
 def test_incomplete_reads():
     # Using undocumented "0 MB" budget to test incomplete reads.
-    uri = os.path.join(TESTS_INPUT_DIR, 'arrays/v3/ingested_2samples')
+    uri = os.path.join(TESTS_INPUT_DIR, "arrays/v3/ingested_2samples")
     cfg = tiledbvcf.ReadConfig(memory_budget_mb=0)
-    test_ds = tiledbvcf.Dataset(uri, mode='r', cfg=cfg)
+    test_ds = tiledbvcf.Dataset(uri, mode="r", cfg=cfg)
 
     expected_df = pd.DataFrame(
-        {'sample_name': pd.Series(
-            ['HG00280', 'HG01762', 'HG00280', 'HG01762', 'HG00280',
-             'HG01762', 'HG00280', 'HG00280', 'HG00280', 'HG00280',
-             'HG00280', 'HG00280', 'HG00280', 'HG00280']),
-            'pos_start': pd.Series(
-                [12141, 12141, 12546, 12546, 13354, 13354, 13375, 13396,
-                 13414, 13452, 13520, 13545, 17319, 17480], dtype=np.int32),
-            'pos_end': pd.Series(
-                [12277, 12277, 12771, 12771, 13374, 13389, 13395, 13413,
-                 13451, 13519, 13544, 13689, 17479, 17486], dtype=np.int32)})
+        {
+            "sample_name": pd.Series(
+                [
+                    "HG00280",
+                    "HG01762",
+                    "HG00280",
+                    "HG01762",
+                    "HG00280",
+                    "HG01762",
+                    "HG00280",
+                    "HG00280",
+                    "HG00280",
+                    "HG00280",
+                    "HG00280",
+                    "HG00280",
+                    "HG00280",
+                    "HG00280",
+                ]
+            ),
+            "pos_start": pd.Series(
+                [
+                    12141,
+                    12141,
+                    12546,
+                    12546,
+                    13354,
+                    13354,
+                    13375,
+                    13396,
+                    13414,
+                    13452,
+                    13520,
+                    13545,
+                    17319,
+                    17480,
+                ],
+                dtype=np.int32,
+            ),
+            "pos_end": pd.Series(
+                [
+                    12277,
+                    12277,
+                    12771,
+                    12771,
+                    13374,
+                    13389,
+                    13395,
+                    13413,
+                    13451,
+                    13519,
+                    13544,
+                    13689,
+                    17479,
+                    17486,
+                ],
+                dtype=np.int32,
+            ),
+        }
+    )
 
     # Region partitions
-    dask_df = test_ds.read_dask(attrs=['sample_name', 'pos_start', 'pos_end'],
-                                region_partitions=10)
+    dask_df = test_ds.read_dask(
+        attrs=["sample_name", "pos_start", "pos_end"], region_partitions=10
+    )
     df = dask_df.compute()
     _check_dfs(expected_df, df)
 
     # Sample partitions (we have to sort to check the result)
-    dask_df = test_ds.read_dask(attrs=['sample_name', 'pos_start', 'pos_end'],
-                                sample_partitions=2)
-    df = dask_df.compute().sort_values('sample_name').reset_index(drop=True)
-    _check_dfs(expected_df.sort_values('sample_name').reset_index(drop=True),
-               df)
+    dask_df = test_ds.read_dask(
+        attrs=["sample_name", "pos_start", "pos_end"], sample_partitions=2
+    )
+    df = dask_df.compute().sort_values("sample_name").reset_index(drop=True)
+    _check_dfs(expected_df.sort_values("sample_name").reset_index(drop=True), df)
 
     # Both
-    dask_df = test_ds.read_dask(attrs=['sample_name', 'pos_start', 'pos_end'],
-                                region_partitions=10, sample_partitions=2)
-    df = dask_df.compute().sort_values('sample_name').reset_index(drop=True)
-    _check_dfs(expected_df.sort_values('sample_name').reset_index(drop=True),
-               df)
+    dask_df = test_ds.read_dask(
+        attrs=["sample_name", "pos_start", "pos_end"],
+        region_partitions=10,
+        sample_partitions=2,
+    )
+    df = dask_df.compute().sort_values("sample_name").reset_index(drop=True)
+    _check_dfs(expected_df.sort_values("sample_name").reset_index(drop=True), df)
 
     # No partitioning
-    dask_df = test_ds.read_dask(attrs=['sample_name', 'pos_start', 'pos_end'])
+    dask_df = test_ds.read_dask(attrs=["sample_name", "pos_start", "pos_end"])
     df = dask_df.compute()
     _check_dfs(expected_df, df)
 
     # Subset of partitions (limit_partitions)
-    dask_df = test_ds.read_dask(attrs=['sample_name', 'pos_start', 'pos_end'],
-                                region_partitions=10, sample_partitions=2,
-                                limit_partitions=2)
+    dask_df = test_ds.read_dask(
+        attrs=["sample_name", "pos_start", "pos_end"],
+        region_partitions=10,
+        sample_partitions=2,
+        limit_partitions=2,
+    )
     assert dask_df.npartitions == 2
 
 
 def test_basic_map(test_ds):
     expected_df = pd.DataFrame(
-        {'sample_name': pd.Series(['HG00280', 'HG01762']),
-         'pos_start': pd.Series([12141, 12141], dtype=np.int32),
-         'pos_end': pd.Series([12277, 12277], dtype=np.int32)})
+        {
+            "sample_name": pd.Series(["HG00280", "HG01762"]),
+            "pos_start": pd.Series([12141, 12141], dtype=np.int32),
+            "pos_end": pd.Series([12277, 12277], dtype=np.int32),
+        }
+    )
 
     # Region partitions
-    dask_df = test_ds.map_dask(lambda df: df[df.pos_start * 2 < 25000],
-                               attrs=['sample_name', 'pos_start', 'pos_end'],
-                               region_partitions=10)
+    dask_df = test_ds.map_dask(
+        lambda df: df[df.pos_start * 2 < 25000],
+        attrs=["sample_name", "pos_start", "pos_end"],
+        region_partitions=10,
+    )
     df = dask_df.compute()
     _check_dfs(expected_df, df)
 
 
 def test_map_incomplete():
     # Using undocumented "0 MB" budget to test incomplete reads.
-    uri = os.path.join(TESTS_INPUT_DIR, 'arrays/v3/ingested_2samples')
+    uri = os.path.join(TESTS_INPUT_DIR, "arrays/v3/ingested_2samples")
     cfg = tiledbvcf.ReadConfig(memory_budget_mb=0)
-    test_ds = tiledbvcf.Dataset(uri, mode='r', cfg=cfg)
+    test_ds = tiledbvcf.Dataset(uri, mode="r", cfg=cfg)
 
     expected_df = pd.DataFrame(
-        {'sample_name': pd.Series(['HG00280', 'HG01762']),
-         'pos_start': pd.Series([12141, 12141], dtype=np.int32),
-         'pos_end': pd.Series([12277, 12277], dtype=np.int32)})
+        {
+            "sample_name": pd.Series(["HG00280", "HG01762"]),
+            "pos_start": pd.Series([12141, 12141], dtype=np.int32),
+            "pos_end": pd.Series([12277, 12277], dtype=np.int32),
+        }
+    )
 
     # Region partitions
-    dask_df = test_ds.map_dask(lambda df: df[df.pos_start * 2 < 25000],
-                               attrs=['sample_name', 'pos_start', 'pos_end'],
-                               region_partitions=10)
+    dask_df = test_ds.map_dask(
+        lambda df: df[df.pos_start * 2 < 25000],
+        attrs=["sample_name", "pos_start", "pos_end"],
+        region_partitions=10,
+    )
     df = dask_df.compute()
     _check_dfs(expected_df, df)
