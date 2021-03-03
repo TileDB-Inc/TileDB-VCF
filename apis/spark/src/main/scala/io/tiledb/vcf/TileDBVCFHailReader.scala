18f6504df868c2fd0457aa2070094af6cbc1da27
gsvic <victorasgs@gmail.com>
2021-03-03 15:55:58 +0200

    Added TileDBVCFHailReader build methods

--- apis/spark/src/main/scala/io/tiledb/vcf/TileDBVCFHailReader.scala
+++ apis/spark/src/main/scala/io/tiledb/vcf/TileDBVCFHailReader.scala
@@ -1,74 +1,73 @@
 package io.tiledb.vcf
 
-import io.tiledb.libvcfnative.VCFReader
-import io.tiledb.libvcfnative.VCFReader.AttributeDatatype
 import is.hail.annotations.BroadcastRow
-
-import java.net.URI
-import is.hail.expr.SparkAnnotationImpex
 import is.hail.expr.ir.{ExecuteContext, LowerMatrixIR, MatrixHybridReader, TableRead, TableValue}
-import is.hail.io.vcf.MatrixVCFReaderParameters
 import is.hail.rvd.{RVD, RVDType}
 import is.hail.sparkextras.ContextRDD
-import is.hail.types.physical.{PCanonicalCall, PCanonicalString, PFloat32, PInt32, PStruct, PType}
-import is.hail.types.virtual.{TArray, TCall, TLocus, TString, TStruct, Type}
+import is.hail.types.physical.{PCanonicalCall, PCanonicalString, PField, PFloat32, PInt32, PStruct, PType}
+import is.hail.types.virtual._
 import is.hail.types.{MatrixType, TableType}
-import is.hail.utils.{toRichContextRDDRow, toRichIterable}
-import is.hail.variant.{Locus, ReferenceGenome}
-import org.apache.spark.sql.{DataFrame, Row}
-import org.json4s.{DefaultFormats, Formats}
+import is.hail.utils.{FastIndexedSeq, toRichContextRDDRow, toRichIterable}
+import is.hail.variant.{Call, Call2, Locus, ReferenceGenome}
+import org.apache.spark.sql.{Column, DataFrame, Row, functions}
 import org.json4s.JsonAST.JValue
+import org.json4s.{DefaultFormats, Formats}
 
 import java.util.Optional
 import scala.collection.mutable
 
 
-class TileDBHailVCFReader(val uri: String, val samples: String) extends MatrixHybridReader {
+class TileDBHailVCFReader() extends MatrixHybridReader {
   override def pathsUsed: Seq[String] = Seq.empty
   override def columnCount: Option[Int] = None
   override def partitionCounts: Option[IndexedSeq[Long]] = None
 
-  val vcfReader: VCFReader = new VCFReader(uri, samples.split(","), Optional.empty(), Optional.empty())
+  var df: DataFrame = null
+  var uri: String = null
+  var samples: Option[String] = Option.empty
 
-  val fmt = vcfReader.fmtAttributes.keySet.toArray.map{ fmtAttr =>
-    val key = fmtAttr.toString
-    val dt = {
-      if (key.contains("GT")) {
-        PCanonicalCall()
-      }
-      else {
-        vcfReader.fmtAttributes.get(key).datatype match {
-          case AttributeDatatype.UINT8 => PInt32()
-          case AttributeDatatype.INT32 => PInt32()
-          case AttributeDatatype.CHAR => PCanonicalString()
-          case AttributeDatatype.FLOAT32 => PFloat32()
-        }
-      }
-    }
-    (fmtAttr.toString.split("_")(1) -> dt.virtualType)
-  }
+  //  val sampleList = {
+  //    if (samples.isDefined) samples.get.split(",")
+  //    else Array[String]()
+  //  }
+  //
+  //  val vcfReader: VCFReader = new VCFReader(uri, sampleList, Optional.empty(), Optional.empty())
+  //  val fmt = vcfReader.fmtAttributes.keySet.toArray.map{ fmtAttr =>
+  //    val key = fmtAttr.toString
+  //    val dt = {
+  //      if (key.contains("GT")) {
+  //        PCanonicalCall()
+  //      }
+  //      else {
+  //        vcfReader.fmtAttributes.get(key).datatype match {
+  //          case AttributeDatatype.UINT8 => PInt32()
+  //          case AttributeDatatype.INT32 => PInt32()
+  //          case AttributeDatatype.CHAR => PCanonicalString()
+  //          case AttributeDatatype.FLOAT32 => PFloat32()
+  //        }
+  //      }
+  //    }
+  //    (fmtAttr.toString.split("_")(1) -> dt.virtualType)
+  //  }
+  //
+  //  val info = vcfReader.infoAttributes.keySet.toArray.map{ infoAttr =>
+  //    val key = infoAttr.toString
+  //    val dt = {
+  //      if (key.contains("GT"))
+  //        PCanonicalCall(false)
+  //      else {
+  //        vcfReader.infoAttributes.get(key).datatype match {
+  //          case AttributeDatatype.UINT8 => PInt32()
+  //          case AttributeDatatype.INT32 => PInt32()
+  //          case AttributeDatatype.CHAR => PCanonicalString()
+  //          case AttributeDatatype.FLOAT32 => PFloat32()
+  //        }
+  //      }
+  //    }
+  //    (infoAttr.toString.split("_")(1) -> dt.virtualType)
+  //  }
 
-  val info = vcfReader.infoAttributes.keySet.toArray.map{ infoAttr =>
-    val key = infoAttr.toString
-    val dt = {
-      if (key.contains("GT"))
-        PCanonicalCall(false)
-      else {
-        vcfReader.infoAttributes.get(key).datatype match {
-          case AttributeDatatype.UINT8 => PInt32()
-          case AttributeDatatype.INT32 => PInt32()
-          case AttributeDatatype.CHAR => PCanonicalString()
-          case AttributeDatatype.FLOAT32 => PFloat32()
-        }
-      }
-    }
-    (infoAttr.toString.split("_")(1) -> dt.virtualType)
-  }
-
-  //val entryType = TStruct((fmt):_ *)
-  //val entryType = TStruct(Array("GT" -> PCanonicalCall().virtualType):_*)
-  //val entryType = TStruct("GT" -> TCall)
-  val entryType = TStruct("DP" -> PInt32().virtualType, "PL" -> PInt32().virtualType)
+  val entryType = TStruct("DP" -> PInt32().virtualType, "GT" -> PCanonicalCall().virtualType, "PL" -> TArray(PInt32().virtualType))
 
   val fullMatrixType: MatrixType = MatrixType(
     globalType = TStruct.empty,
@@ -76,30 +75,98 @@ class TileDBHailVCFReader(val uri: String, val samples: String) extends MatrixHy
     colKey = Array("s"),
     rowType = TStruct(
       "locus" -> TLocus(ReferenceGenome.GRCh37),
-      "alleles" -> TArray(TString),
-      "info" -> entryType),
+      "alleles" -> TArray(TString)),
     rowKey = Array("locus", "alleles"),
     entryType = entryType)
 
-  val str = ""
-
   override def apply(tr: TableRead, ctx: ExecuteContext): TableValue = {
-    val df = ctx.backend.asSpark("spark")
-      .sparkSession.read
-      .format("io.tiledb.vcf")
-      .option("samples", samples)
-      .option("uri", uri)
-      .load()
+    val requestedType = tr.typ
+    val rowType = tr.typ.rowType
 
-    //val tt = TableType(fullMatrixType.rowType, fullMatrixType.rowType.fieldNames, TStruct.empty)
-    val tt = fullMatrixType.toTableType(LowerMatrixIR.entriesFieldName, LowerMatrixIR.colsFieldName)
-
-    val rdd = df.select("contig", "posStart", "alleles", "fmt_DP", "fmt_PL").rdd.map{
-      //      r => Row(Locus(r.getString(0), r.getInt(1)), r.get(2).asInstanceOf[mutable.WrappedArray[Int]].toIndexedSeq, IndexedSeq(Row(r.get(3), 1, 1, 1, 1, 1, 1)))
-      r => Row(Locus(r.getString(0), r.getInt(1)), r.get(2).asInstanceOf[mutable.WrappedArray[Int]].toIndexedSeq, Row(1, 1))
+    val entryType: TStruct = rowType.fieldOption(LowerMatrixIR.entriesFieldName) match {
+      case Some(entriesArray) => entriesArray.typ.asInstanceOf[TArray].elementType.asInstanceOf[TStruct]
+      case None => TStruct.empty
     }
 
-    TableValue(ctx, tr.typ.rowType, tr.typ.key, rdd)
+    if (df == null) {
+      df = ctx.backend.asSpark("spark")
+        .sparkSession.read
+        .format("io.tiledb.vcf")
+        .option("samples", samples.get)
+        .option("uri", uri)
+        .load()
+    }
+
+    var projection = df.groupBy("contig","posStart", "alleles")
+      .agg(functions.collect_list("genotype").as("GT"),
+        functions.collect_list("fmt_DP").as("DP"),
+        functions.collect_list("fmt_PL").as("PL"))
+
+    var columns = Seq[String]("contig", "posStart", "alleles")
+
+    val infoFields = Seq("GT", "DP", "PL")
+
+    infoFields.foreach(infoField => if (entryType.hasField(infoField)) columns = columns :+ infoField)
+
+    projection = projection.select(columns.map(new Column(_)) :  _ *)
+
+    val rdd = projection.rdd.map{
+      row => {
+        val locus = Locus(row.getString(0), row.getInt(1))
+        val alleles = row.get(2)
+
+        var rowFields = Seq(locus, alleles)
+        
+        // Parse Entry fields (GT, DP and PL)
+        var idx = rowFields.size + 1
+        entryType.fieldNames.foreach { fieldName =>
+          fieldName match {
+            case "GT" => {
+              val GT = row.get(idx).asInstanceOf[mutable.WrappedArray[mutable.WrappedArray[Int]]]
+                .map(x => Row(Call2(x(0), x(1))))
+              rowFields = rowFields :+ GT
+            }
+            case "DP" => {
+              val DP = row.get(idx).asInstanceOf[mutable.WrappedArray[Int]]
+                .map(x => Row(x))
+              rowFields = rowFields :+ DP
+            }
+            case "PL" => {
+              val PL = row.get(idx).asInstanceOf[mutable.WrappedArray[mutable.WrappedArray[Int]]]
+                .map(x => Row(x))
+              rowFields = rowFields :+ PL
+            }
+            case _ => {}
+          }
+          idx += 1
+        }
+
+        Row(rowFields : _ *)
+      }
+    }
+
+    val tt = fullMatrixType.toTableType(LowerMatrixIR.entriesFieldName, LowerMatrixIR.colsFieldName)
+
+    val globals = {
+      if (samples.isDefined)
+        Row(samples.get.split(",").map(Row(_)).toFastIndexedSeq)
+      else
+        Row(FastIndexedSeq.empty)
+    }
+    val g = { (requestedGlobalsType: Type) =>
+      val subset = tt.globalType.valueSubsetter(requestedGlobalsType)
+      subset(globals).asInstanceOf[Row]
+    }
+
+    val crdd = ContextRDD.weaken(rdd).toRegionValues(requestedType.canonicalRowPType)
+    val rvd = RVD.coerce(ctx, requestedType.canonicalRVDType, crdd)
+
+    val tv = TableValue(ctx,
+      requestedType,
+      BroadcastRow(ctx, g(requestedType.globalType), requestedType.globalType),
+      rvd)
+
+    tv
   }
 
   override def rowAndGlobalPTypes(ctx: ExecuteContext, requestedType: TableType): (PStruct, PStruct) = {
@@ -114,10 +181,29 @@ object TileDBHailVCFReader {
     implicit val formats: Formats = DefaultFormats
     val params = jv.extract[TileDBHail]
 
-    new TileDBHailVCFReader(params.uri, params.samples)
+    val reader = new TileDBHailVCFReader()
+
+    reader.uri = params.uri
+    reader.samples = Option(params.samples)
+
   }
 
-  def apply(ctx: ExecuteContext, uri: String, samples: String): TileDBHailVCFReader = {
-    new TileDBHailVCFReader(uri, samples)
+  def build(uri: String, samples: Option[String]): TileDBHailVCFReader = {
+    val reader = new TileDBHailVCFReader
+    reader.uri = uri
+    reader.samples = samples
+
+    reader
+  }
+
+  def build(df: DataFrame): TileDBHailVCFReader = {
+    val reader = new TileDBHailVCFReader()
+    reader.df = df
+
+    reader
+  }
+
+  def build(uri: String): TileDBHailVCFReader = {
+    build(uri, Option.empty)
   }
 }
--- apis/spark/src/test/java/io/tiledb/vcf/HailReaderTests.java
+++ apis/spark/src/test/java/io/tiledb/vcf/HailReaderTests.java
@@ -14,21 +14,20 @@ import is.hail.io.fs.HadoopFS;
 import is.hail.utils.ExecutionTimer;
 import is.hail.utils.SerializableHadoopConfiguration;
 import is.hail.variant.Locus;
-import java.net.URI;
+
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
+
 import org.apache.hadoop.conf.Configuration;
 import org.apache.log4j.Level;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.catalyst.expressions.GenericRow;
-import org.apache.spark.sql.sources.v2.DataSourceOptions;
 import org.junit.Assert;
 import org.junit.Test;
-import scala.collection.JavaConversions;
+
+import io.tiledb.vcf.TileDBHailVCFReader;
 
 public class HailReaderTests extends SharedJavaSparkSession {
 
@@ -47,13 +46,13 @@ public class HailReaderTests extends SharedJavaSparkSession {
     return "file://".concat(path.toAbsolutePath().toString());
   }
 
-  private Dataset<Row> testSampleDataset() {
+  private Dataset<Row> testSampleDataset(String samples) {
     Dataset<Row> dfRead =
         session()
             .read()
             .format("io.tiledb.vcf")
             .option("uri", testSampleGroupURI("ingested_2samples"))
-            .option("samples", "HG01762,HG00280")
+            .option("samples", samples)
             .option("ranges", "1:12100-13360,1:13500-17350")
             .option("tiledb.vfs.num_threads", 1)
             .option("tiledb_stats_log_level", Level.INFO.toString())
@@ -63,31 +62,23 @@ public class HailReaderTests extends SharedJavaSparkSession {
 
   @Test
   public void hailReader() {
-    Map<String, String> optionsMap = new HashMap<>();
-    optionsMap.put("uri", testSampleGroupURI("ingested_2samples"));
-    optionsMap.put("samples", "HG01762,HG00280");
-    optionsMap.put("ranges", "1:12100-13360,1:13500-17350");
-    optionsMap.put("partitions", "10");
-    optionsMap.put("tiledb.vfs.num_threads", "1");
-    optionsMap.put("tiledb_stats_log_level", Level.INFO.toString());
-
-    DataSourceOptions dataSourceOptions = new DataSourceOptions(optionsMap);
-    VCFDataSourceOptions opts = new VCFDataSourceOptions(dataSourceOptions);
-
     SparkBackend sb = new SparkBackend("/tmp", "file:///tmp", sparkSession.sparkContext());
     HailContext hc = HailContext.apply(sb, "/tmp/hail.log", false, false, 50, false, 3);
 
-    TileDBHailVCFReader reader =
-        new TileDBHailVCFReader(testSampleGroupURI("ingested_2samples"), "HG01762,HG00280");
-
     ExecuteContext ctx =
-        new ExecuteContext(
-            "/tmp",
-            "file:///tmp",
-            hc.backend(),
-            new HadoopFS(new SerializableHadoopConfiguration(new Configuration())),
-            Region.apply(Region.REGULAR(), RegionPool.apply(false)),
-            new ExecutionTimer("HailSuite"));
+            new ExecuteContext(
+                    "/tmp",
+                    "file:///tmp",
+                    hc.backend(),
+                    new HadoopFS(new SerializableHadoopConfiguration(new Configuration())),
+                    Region.apply(Region.REGULAR(), RegionPool.apply(false)),
+                    new ExecutionTimer("HailSuite"));
+
+    // Test with sample HG01762
+    String sample = "HG01762";
+
+    TileDBHailVCFReader reader =
+        TileDBHailVCFReader.build(testSampleDataset(sample));
 
     MatrixRead r = new MatrixRead(reader.fullMatrixType(), false, false, reader);
     TableIR mt = new MatrixRowsTable(r);
@@ -96,85 +87,29 @@ public class HailReaderTests extends SharedJavaSparkSession {
 
     Row[] rows = (Row[]) tv.rvd().toRows().collect();
 
-    Row[] expectedRows =
-        new GenericRow[] {
-          new GenericRow(
-              new Object[] {
-                Locus.apply("1", 12141),
-                JavaConversions.asScalaBuffer(Arrays.asList(new Object[] {"C", "<NON_REF>"}))
-                    .toSeq()
-              }),
-          new GenericRow(
-              new Object[] {
-                Locus.apply("1", 12141),
-                JavaConversions.asScalaBuffer(Arrays.asList(new Object[] {"C", "<NON_REF>"}))
-                    .toSeq()
-              }),
-          new GenericRow(
-              new Object[] {
-                Locus.apply("1", 12546),
-                JavaConversions.asScalaBuffer(Arrays.asList(new Object[] {"G", "<NON_REF>"}))
-                    .toSeq()
-              }),
-          new GenericRow(
-              new Object[] {
-                Locus.apply("1", 12546),
-                JavaConversions.asScalaBuffer(Arrays.asList(new Object[] {"G", "<NON_REF>"}))
-                    .toSeq()
-              }),
-          new GenericRow(
-              new Object[] {
-                Locus.apply("1", 13354),
-                JavaConversions.asScalaBuffer(Arrays.asList(new Object[] {"T", "<NON_REF>"}))
-                    .toSeq()
-              }),
-          new GenericRow(
-              new Object[] {
-                Locus.apply("1", 13354),
-                JavaConversions.asScalaBuffer(Arrays.asList(new Object[] {"T", "<NON_REF>"}))
-                    .toSeq()
-              }),
-          new GenericRow(
-              new Object[] {
-                Locus.apply("1", 13452),
-                JavaConversions.asScalaBuffer(Arrays.asList(new Object[] {"G", "<NON_REF>"}))
-                    .toSeq()
-              }),
-          new GenericRow(
-              new Object[] {
-                Locus.apply("1", 13520),
-                JavaConversions.asScalaBuffer(Arrays.asList(new Object[] {"G", "<NON_REF>"}))
-                    .toSeq()
-              }),
-          new GenericRow(
-              new Object[] {
-                Locus.apply("1", 13545),
-                JavaConversions.asScalaBuffer(Arrays.asList(new Object[] {"G", "<NON_REF>"}))
-                    .toSeq()
-              }),
-          new GenericRow(
-              new Object[] {
-                Locus.apply("1", 17319),
-                JavaConversions.asScalaBuffer(Arrays.asList(new Object[] {"T", "<NON_REF>"}))
-                    .toSeq()
-              }),
-        };
+    List<GenericRow> expectedRows= testSampleDataset(sample).select("contig", "posStart", "alleles").javaRDD().map(x -> {
+      return new GenericRow(new Object[]{Locus.apply((String)x.get(0), (int)x.get(1)), x.get(2)});
+    }).collect();
 
-    System.out.println("Expected");
-    for (Row row : expectedRows) {
-      System.out.println(row);
-    }
+    Assert.assertArrayEquals(expectedRows.toArray(), rows);
 
-    System.out.println("Actual");
-    for (Row row : rows) {
-      System.out.println(row);
-    }
+    // Test sample HG00280
+    sample = "HG00280";
 
-    Assert.assertArrayEquals(expectedRows, rows);
+    reader =
+            TileDBHailVCFReader.build(testSampleDataset(sample));
 
-    for (Row row : rows) {
-      System.out.println(row.getClass());
-      System.out.println(row.toString());
-    }
+    r = new MatrixRead(reader.fullMatrixType(), false, false, reader);
+    mt = new MatrixRowsTable(r);
+
+    tv = Interpret.apply(mt, ctx);
+
+    rows = (Row[]) tv.rvd().toRows().collect();
+
+    expectedRows= testSampleDataset(sample).select("contig", "posStart", "alleles").javaRDD().map(x -> {
+      return new GenericRow(new Object[]{Locus.apply((String)x.get(0), (int)x.get(1)), x.get(2)});
+    }).collect();
+
+    Assert.assertArrayEquals(expectedRows.toArray(), rows);
   }
 }
