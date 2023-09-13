086f27e76f9107093cc870bfa25b26cd3c3cb097
John Blischak <jdblischak@gmail.com>
2023-09-13 15:54:35 -0400

    Add nightly build (linux only for now)

---
+++ ci/nightly/build-libtiledb.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -ex
+
+# Build libtiledb assuming source code directory is ./TileDB
+
+cmake -S TileDB -B build-libtiledb \
+  -D CMAKE_BUILD_TYPE=Release \
+  -D CMAKE_INSTALL_PREFIX:PATH=$GITHUB_WORKSPACE/install/ \
+  -D TILEDB_WERROR=ON \
+  -D TILEDB_SERIALIZATION=ON \
+  -D TILEDB_VCPKG=OFF
+
+cmake --build build-libtiledb -j2 --config Release
+
+cmake --build build-libtiledb --config Release --target install-tiledb
---
+++ ci/nightly/build-tiledbvcf-py.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -ex
+
+# Build tiledbvcf-py assuming source code directory is ./TileDB-VCF/apis/python
+
+export LD_LIBRARY_PATH=$GITHUB_WORKSPACE/install/lib:${LD_LIBRARY_PATH-}
+echo "LD_LIBRARY_PATH: $LD_LIBRARY_PATH"
+
+cd TileDB-VCF/apis/python
+python setup.py install --libtiledbvcf=$GITHUB_WORKSPACE/install/
+python -c "import tiledbvcf; print(tiledbvcf.version)"
