b1f158df8ee3efa4acaece7a9c88f602142efa6e
John Blischak <jdblischak@gmail.com>
2023-09-19 11:17:09 -0400

    Nightly build for macOS

--- ci/nightly/build-tiledbvcf-py.sh
+++ ci/nightly/build-tiledbvcf-py.sh
@@ -5,8 +5,17 @@ set -ex
 # ./TileDB-VCF/apis/python and libtiledbvcf shared library installed in
 # $GITHUB_WORKSPACE/install/
 
-export LD_LIBRARY_PATH=$GITHUB_WORKSPACE/install/lib:${LD_LIBRARY_PATH-}
-echo "LD_LIBRARY_PATH: $LD_LIBRARY_PATH"
+OS=$(uname)
+echo "OS: $OS"
+if [[ $OS == Linux ]]
+then
+  export LD_LIBRARY_PATH=$GITHUB_WORKSPACE/install/lib:${LD_LIBRARY_PATH-}
+  echo "LD_LIBRARY_PATH: $LD_LIBRARY_PATH"
+elif [[ $OS == Darwin ]]
+then
+  export DYLD_LIBRARY_PATH=$GITHUB_WORKSPACE/install/lib:${DYLD_LIBRARY_PATH-}
+  echo "DYLD_LIBRARY_PATH: $DYLD_LIBRARY_PATH"
+fi
 
 export LIBTILEDBVCF_PATH=$GITHUB_WORKSPACE/install/
 
