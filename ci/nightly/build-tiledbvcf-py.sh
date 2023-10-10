6962d749ae3ceb126e5146d4ab66e35ccc093c34
John Blischak <jdblischak@gmail.com>
2023-10-10 10:23:10 -0400

    Nightly build for macOS (#579)
    * Nightly build for macOS
    
    * Switch to pip for installation
    
    * Bump macOS image from 11 to 12
    
    https://github.com/TileDB-Inc/TileDB/pull/4367
    
    * Pin fsspec<2023.3.0
    
    https://github.com/TileDB-Inc/TileDB-VCF/commit/a53ef3308c5a7380e9102260e4327562aeaf51be
    
    * Install Pythons dependencies with PyPI wheels on macOS
    
    * Use $CONDA instead of $CONDA_PREFIX

--- ci/nightly/build-tiledbvcf-py.sh
+++ ci/nightly/build-tiledbvcf-py.sh
@@ -5,13 +5,22 @@ set -ex
 # ./TileDB-VCF/apis/python and libtiledbvcf shared library installed in
 # $GITHUB_WORKSPACE/install/
 
-export LD_LIBRARY_PATH=$GITHUB_WORKSPACE/install/lib:${LD_LIBRARY_PATH-}
-echo "LD_LIBRARY_PATH: $LD_LIBRARY_PATH"
+OS=$(uname)
+echo "OS: $OS"
+if [[ $OS == Linux ]]
+then
+  export LD_LIBRARY_PATH=$GITHUB_WORKSPACE/install/lib:${LD_LIBRARY_PATH-}
+  echo "LD_LIBRARY_PATH: $LD_LIBRARY_PATH"
+elif [[ $OS == Darwin ]]
+then
+  export DYLD_LIBRARY_PATH=$GITHUB_WORKSPACE/install/lib:${DYLD_LIBRARY_PATH-}
+  echo "DYLD_LIBRARY_PATH: $DYLD_LIBRARY_PATH"
+fi
 
 export LIBTILEDBVCF_PATH=$GITHUB_WORKSPACE/install/
 
 cd TileDB-VCF/apis/python
-python setup.py install
+python -m pip install .
 python -c "import tiledbvcf; print(tiledbvcf.version)"
 
 pytest
