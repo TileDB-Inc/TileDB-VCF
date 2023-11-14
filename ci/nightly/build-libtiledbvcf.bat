832dfd94dadc3803acd87adfdc89104ab8f04277
John Blischak <jdblischak@gmail.com>
2023-11-14 12:35:17 -0500

    Define CMAKE_INSTALL_LIBDIR to install .dll in bin/ next to .exe

--- ci/nightly/build-libtiledbvcf.bat
+++ ci/nightly/build-libtiledbvcf.bat
@@ -6,7 +6,8 @@ cmake -S TileDB-VCF\libtiledbvcf -B build-libtiledbvcf ^
   -D CMAKE_BUILD_TYPE=Release ^
   -D CMAKE_INSTALL_PREFIX:PATH=%GITHUB_WORKSPACE%\install\ ^
   -D OVERRIDE_INSTALL_PREFIX=OFF ^
-  -D TILEDB_WERROR=OFF
+  -D TILEDB_WERROR=OFF ^
+  -D CMAKE_INSTALL_LIBDIR=bin
 if %ERRORLEVEL% neq 0 exit 1
 
 cmake --build build-libtiledbvcf -j2 --config Release
@@ -15,5 +16,7 @@ if %ERRORLEVEL% neq 0 exit 1
 cmake --build build-libtiledbvcf --config Release --target install-libtiledbvcf
 if %ERRORLEVEL% neq 0 exit 1
 
-rem %GITHUB_WORKSPACE%\install\bin\tiledbvcf version
+dir %GITHUB_WORKSPACE%\install\
+
+%GITHUB_WORKSPACE%\install\bin\tiledbvcf version
 rem if %ERRORLEVEL% neq 0 exit 1
