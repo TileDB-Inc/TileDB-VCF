1fa0e51093409cde7fbc22af5c3419d980c5305c
George Powley <george.powley@gmail.com>
2024-03-14 15:13:18 -0400

    update to tiledb 2.21.0

--- scripts/update-tiledb-version.py
+++ scripts/update-tiledb-version.py
@@ -4,7 +4,6 @@ import argparse
 import hashlib
 import os
 import re
-from fileinput import FileInput
 from subprocess import run
 from urllib.request import urlopen
 
@@ -12,14 +11,19 @@ from urllib.request import urlopen
 def hash_url_file(url):
     """Return SHA1 hash of the file located at the provided url."""
 
-    BLOCK_SIZE = 65536
-    hash = hashlib.sha1()
-    with urlopen(url) as fp:
-        while True:
-            data = fp.read(BLOCK_SIZE)
-            if not data:
-                return hash.hexdigest()
-            hash.update(data)
+    try:
+        print(f"  hashing {url}")
+        BLOCK_SIZE = 65536
+        hash = hashlib.sha1()
+        with urlopen(url) as fp:
+            while True:
+                data = fp.read(BLOCK_SIZE)
+                if not data:
+                    return hash.hexdigest()
+                hash.update(data)
+    except Exception as e:
+        print(f"Error: failed to hash file at {url}")
+        raise
 
 
 def get_version_hash(version):
@@ -40,31 +44,32 @@ def main(args):
     old_hash = None
     sha1 = None
 
-    new_hash = get_version_hash(args.version)
+    new_version = args.version
+    new_hash = get_version_hash(new_version)
 
     filepath = (
         f"{os.path.dirname(__file__)}/../libtiledbvcf/cmake/Modules/FindTileDB_EP.cmake"
     )
-    filepath = os.path.realpath(filepath)
     print(f"Updating {filepath}")
-    print(f"  new version = {args.version}-{new_hash}")
-    print("  computing SHA1 hashes...")
+    print(f"  new version = {new_version}-{new_hash}")
 
-    # all "print" statements in this "with" block go to the "fp" file
-    with FileInput(filepath, inplace=True) as fp:
+    findtiledb_lines = []
+    with open(filepath) as fp:
         for line in fp:
             line = line.rstrip()
 
+            # Find the old version and hash value
             if old_version is None:
                 m = re.search(r"TileDB/releases/download/(.*?)/.*-(.*)\.zip", line)
                 if m:
                     old_version = m.group(1)
                     old_hash = m.group(2)
+                    print(f"  old version = {old_version}-{old_hash}")
 
             if old_version is not None:
                 # modify url
                 if "https://" in line:
-                    line = line.replace(old_version, args.version)
+                    line = line.replace(old_version, new_version)
                     line = line.replace(old_hash, new_hash)
 
                 # update sha1 value computed on previous line
@@ -79,10 +84,13 @@ def main(args):
                     if m:
                         sha1 = hash_url_file(m.group(1))
 
-            # print line to file
-            print(line)
+            # Add line to the list of lines to write
+            findtiledb_lines.append(line)
 
-    print(f"  old version = {old_version}-{old_hash}")
+    # Write new lines to file
+    with open(filepath, "w") as fp:
+        for line in findtiledb_lines:
+            print(line, file=fp)
 
 
 if __name__ == "__main__":
