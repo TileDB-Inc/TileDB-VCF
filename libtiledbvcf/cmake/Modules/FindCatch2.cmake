8bed3e42efb1cbe743e13d4a635317ba1ecdcbd5
George Powley <george.powley@gmail.com>
2024-04-15 00:11:50 -0400

    set LIBTILEDBVCF_DIR; reduce build and test noise

--- libtiledbvcf/cmake/Modules/FindCatch2.cmake
+++ libtiledbvcf/cmake/Modules/FindCatch2.cmake
@@ -34,7 +34,6 @@
 include(TileDBCommon)
 
 # Search the path set during the superbuild for the EP.
-message(STATUS "searching for catch in ${EP_SOURCE_DIR}")
 set(CATCH_PATHS ${EP_SOURCE_DIR}/ep_catch/single_include)
 
 if (NOT TILEDB_FORCE_ALL_DEPS OR TILEDB_CATCH_EP_BUILT)
