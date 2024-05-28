09afd34bf62de8f5247b6df4fecc18a6a7a102fa
George Powley <george.powley@gmail.com>
2024-05-28 14:33:58 -0400

    Lazily import dask (#724)

--- apis/python/src/tiledbvcf/__init__.py
+++ apis/python/src/tiledbvcf/__init__.py
@@ -11,13 +11,6 @@ import tiledbvcf.libtiledbvcf
 # Load basic modules first
 from .dataset import ReadConfig, TileDBVCFDataset, Dataset, config_logging
 
-# Load dask additions, if dask is available
-try:
-    import dask
-    from . import dask_functions
-except ImportError:
-    pass
-
 from .version import version
 from .allele_frequency import read_allele_frequency
 from .sample_qc import sample_qc
