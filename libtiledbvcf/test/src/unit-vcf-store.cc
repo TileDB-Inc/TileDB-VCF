765449259b095c6a674a6227f6add4c3d16cef71
Dušan Baran <me.dusanbaran@gmail.com>
2024-12-03 12:44:29 +0100

    Use correct catch2 header

--- libtiledbvcf/test/src/unit-vcf-store.cc
+++ libtiledbvcf/test/src/unit-vcf-store.cc
@@ -30,7 +30,7 @@
  * Tests for VCF ingestion.
  */
 
-#include <catch2/catch_test_macros.hpp>
+#include <catch2/catch.hpp>
 
 #include "dataset/tiledbvcfdataset.h"
 #include "write/writer.h"
--- libtiledbvcf/test/src/unit-vcf-utils.cc
+++ libtiledbvcf/test/src/unit-vcf-utils.cc
@@ -30,7 +30,7 @@
  * Tests for VCF export.
  */
 
-#include <catch2/catch_test_macros.hpp>
+#include <catch2/catch.hpp>
 
 #include "dataset/tiledbvcfdataset.h"
 #include "read/reader.h"
