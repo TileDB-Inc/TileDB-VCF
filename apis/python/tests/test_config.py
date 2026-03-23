1880175c9a82f148255ae079f7517d92e265d367
Alan Cleary <alan@tiledb.com>
2026-03-23 13:32:06 -0600

    Add tests to cover gaps in testing of ReadConfig parameters
    The previously untested parameters include sort_regions, buffer_percentage, tiledb_tile_cache_percentage, and tiledb_config supplied as both a dict and a tiledb.Config object.

--- apis/python/tests/test_config.py
+++ apis/python/tests/test_config.py
@@ -1,6 +1,7 @@
 import os
 
 import pytest
+import tiledb
 import tiledbvcf
 
 from .conftest import TESTS_INPUT_DIR
@@ -156,6 +157,52 @@ def test_sample_and_region_partitioned_read():
         regions=["1:12000-13000", "1:17000-18000"],
     )
     assert len(df) == 0
+
+
+def test_sort_regions():
+    """sort_regions=False is accepted and returns the same records as sort_regions=True."""
+    uri = os.path.join(TESTS_INPUT_DIR, "arrays/v3/ingested_2samples")
+    regions = ["1:17000-18000", "1:12000-13000"]  # intentionally out of order
+
+    cfg_sorted = tiledbvcf.ReadConfig(sort_regions=True)
+    ds = tiledbvcf.Dataset(uri, mode="r", cfg=cfg_sorted)
+    df_sorted = ds.read(attrs=["sample_name", "pos_start"], regions=regions)
+
+    cfg_unsorted = tiledbvcf.ReadConfig(sort_regions=False)
+    ds = tiledbvcf.Dataset(uri, mode="r", cfg=cfg_unsorted)
+    df_unsorted = ds.read(attrs=["sample_name", "pos_start"], regions=regions)
+
+    assert len(df_sorted) == len(df_unsorted)
+
+
+def test_buffer_percentage_and_tile_cache_percentage():
+    """Non-default buffer_percentage and tiledb_tile_cache_percentage are accepted."""
+    uri = os.path.join(TESTS_INPUT_DIR, "arrays/v3/ingested_2samples")
+    cfg = tiledbvcf.ReadConfig(buffer_percentage=30, tiledb_tile_cache_percentage=5)
+    ds = tiledbvcf.Dataset(uri, mode="r", cfg=cfg)
+    df = ds.read(attrs=["sample_name", "pos_start"], regions=["1:12000-13000"])
+    assert len(df) > 0
+
+
+def test_tiledb_config_as_dict():
+    """tiledb_config can be supplied as a dict instead of a list of strings."""
+    uri = os.path.join(TESTS_INPUT_DIR, "arrays/v3/ingested_2samples")
+    cfg = tiledbvcf.ReadConfig(
+        tiledb_config={"sm.tile_cache_size": "0", "sm.compute_concurrency_level": "1"}
+    )
+    ds = tiledbvcf.Dataset(uri, mode="r", cfg=cfg)
+    df = ds.read(attrs=["sample_name", "pos_start"], regions=["1:12000-13000"])
+    assert len(df) > 0
+
+
+def test_tiledb_config_as_tiledb_config_object():
+    """tiledb_config can be supplied as a tiledb.Config object."""
+    uri = os.path.join(TESTS_INPUT_DIR, "arrays/v3/ingested_2samples")
+    tiledb_cfg = tiledb.Config({"sm.tile_cache_size": "0"})
+    cfg = tiledbvcf.ReadConfig(tiledb_config=tiledb_cfg)
+    ds = tiledbvcf.Dataset(uri, mode="r", cfg=cfg)
+    df = ds.read(attrs=["sample_name", "pos_start"], regions=["1:12000-13000"])
+    assert len(df) > 0
 
 
 @pytest.mark.skipif(os.environ.get("CI") != "true", reason="CI only")
